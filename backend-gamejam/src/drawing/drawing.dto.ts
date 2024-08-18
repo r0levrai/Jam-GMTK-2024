81a58b87cc50169ceaaaa6254f6c1255a1d26713
daniel <daniel.doughan@univ-lyon2.fr>
2024-08-19 00:42:28 +0200

    class validator

--- backend-gamejam/src/drawing/drawing.dto.ts
+++ backend-gamejam/src/drawing/drawing.dto.ts
@@ -1,11 +1,23 @@
+import { IsNotEmpty, IsOptional } from "class-validator";
 import { Points, Vector3 } from "./drawing.entity";
 
 export class DrawingDto {
+    @IsNotEmpty()
     linesPoints: Points[];
+    @IsNotEmpty()
     linesWidth: number[];
+    @IsNotEmpty()
     linesColorIndex: number[];
+
+    @IsOptional()
     userName: string;
+
+    @IsOptional()
     drawingName: string;
+
+    @IsOptional()
     background: string;
+
+    @IsOptional()
     score: number;
 }
