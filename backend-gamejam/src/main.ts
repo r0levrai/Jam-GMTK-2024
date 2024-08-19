cfac38e429456dafb7aa4f7a866d5edf8273010a
daniel <daniel.doughan@univ-lyon2.fr>
2024-08-19 03:38:27 +0200

    add ssl

--- backend-gamejam/src/main.ts
+++ backend-gamejam/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/home/khaled/ssl-certs/private.key'),
+    cert: fs.readFileSync('/home/khaled/ssl-certs/certificate.crt'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.enableCors();
   await app.listen(3000, '0.0.0.0');
