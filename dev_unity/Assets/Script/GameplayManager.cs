930dd9acacc488488ab28334be32e9dff5a0866f
Doug <ddoughan@dev-id.fr>
2024-08-20 19:29:35 +0200

    fix music ingame

--- dev_unity/Assets/Script/GameplayManager.cs
+++ dev_unity/Assets/Script/GameplayManager.cs
@@ -54,6 +54,7 @@ public class GameplayManager : MonoBehaviour
 		bananaFace = bananaRating.Q<VisualElement>("banana");
 		bananaScale = bananaRating.Q<Label>("bananaScale");
 		bananaCommentary = bananaRating.Q<Label>("bananaCommentary");
+		SoundManager.Instance.PlayMusicWithFade("space");
 
 		playerSize.localScale = Vector3.zero;
 		trueSize.localScale = Vector3.zero;
@@ -182,23 +183,23 @@ public class GameplayManager : MonoBehaviour
 		string newSize;
 		if (currentSize / 1000f > 1)
         {
-			newSize = $"{String.Format("{0:0.#}", (currentSize / 1000f))} km";
+			newSize = $"{(currentSize / 1000f)} km";
 		}
 		else if (currentSize / 1f > 1)
 		{
-			newSize = $"{String.Format("{0:0.#}", (currentSize))} m";
+			newSize = $"{(currentSize)} m";
 		}
 		else if (currentSize / 0.01f > 1)
 		{
-			newSize = $"{String.Format("{0:0.#}", (currentSize / 0.01f))} cm";
+			newSize = $"{(currentSize / 0.01f)} cm";
 		}
 		else if (currentSize / 0.001f > 1)
 		{
-			newSize = $"{String.Format("{0:0.#}", (currentSize / 0.001f))} mm";
+			newSize = $"{(currentSize / 0.001f)} mm";
 		}
 		else
         {
-			newSize = $"{String.Format("{0:0.#}", (currentSize / 0.000001f))} nm";
+			newSize = $"{(currentSize / 0.000001f)} nm";
 		}
 		return newSize;
 	}
